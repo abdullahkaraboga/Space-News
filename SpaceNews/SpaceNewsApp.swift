7bf54c472fc2f7df97f9b4c36420540ba66258a1
abdullahkaraboga <abdullahkaraboga@icloud.com>
2023-02-10 00:02:23 +0300

    Initial Commit

---
+++ SpaceNews/SpaceNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceNewsApp.swift
+//  SpaceNews
+//
+//  Created by Abdullah Karaboğa on 10.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct SpaceNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
